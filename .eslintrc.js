2d98110bdaccd90ec0e7f700d44afb36512fda7d
João Pedro Vaz <jonwify@gmail.com>
2022-07-30 17:47:32 -0300

    This is where it all begins...

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'node': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:vue/vue3-recommended',
+        'plugin:vue/vue3-essential',
+        'plugin:vue/vue3-strongly-recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'parser': '@typescript-eslint/parser',
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'vue',
+        '@typescript-eslint'
+    ],
+    'parser': 'vue-eslint-parser',
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
+};
