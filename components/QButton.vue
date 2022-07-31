162821b9999de13da91d7efda0f809cc775539cf
João Pedro Vaz <jonwify@gmail.com>
2022-07-31 20:11:44 -0300

    feat: create button component

---
+++ components/QButton.vue
@@ -0,0 +1,49 @@
+<template>
+  <button
+    :class="{
+      'q-button': true,
+      [`q-button--${variant}`]: variant
+    }"
+  >
+    <slot />
+  </button>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  variant: {
+    type: String,
+    default: 'default',
+    validator: (value: string) => [
+      'default',
+      'contained',
+    ].includes(value),
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.q-button {
+  background-color: transparent;
+  border-radius: 4px;
+  border: 1px solid;
+  cursor: pointer;
+  font-size: var(--font-size-large);
+  font-weight: 700;
+  padding: var(--spacing-sm) var(--spacing-md);
+
+  &:hover {
+    filter: brightness(0.8);
+  }
+
+  &.q-button--default {
+    color: var(--color-primary-dark);
+  }
+
+  &.q-button--contained {
+    background-color: var(--color-secondary-main);
+    border-color: var(--color-secondary-dark);
+    color: var(--color-text-primary);
+  }
+}
+</style>
