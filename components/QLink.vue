fc798ea402af765f2a8200d639bc2c7116794568
João Pedro Vaz <jonwify@gmail.com>
2022-08-01 14:19:38 -0300

    feat: create link component

---
+++ components/QLink.vue
@@ -0,0 +1,17 @@
+<template>
+  <NuxtLink class="q-link">
+    <slot />
+  </NuxtLink>
+</template>
+
+<style lang="scss" scoped>
+.q-link {
+  color: var(--color-primary-dark);
+  font-size: var(--font-size-body1);
+  font-weight: 700;
+
+  &:hover {
+    text-decoration: underline;
+  }
+}
+</style>
