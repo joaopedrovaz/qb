f577c6a16450769b53c69913c8aeea7d8da739de
João Pedro Vaz <jonwify@gmail.com>
2022-08-09 11:21:22 -0300

    feat: create card component

---
+++ components/QCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="q-card">
+    <div class="q-card__content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/theme/mixins/media-query' as mq;
+
+.q-card {
+  background-color: var(--color-common-white);
+  box-shadow: 0 2px 4px rgb(0 0 0 / 20%);
+  position: relative;
+
+  .q-card__content {
+    height: 100%;
+    padding: var(--spacing-md);
+    position: relative;
+    width: 100%;
+  }
+
+  @include mq.media-query('lg') {
+    .q-card__content {
+      padding: var(--spacing-lg);
+    }
+  }
+}
+</style>
